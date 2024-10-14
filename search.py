1dfb258d9dac45c2d838a6fd12d2fadec9fb636c
Shivang Kaul <shangkaul@Shivangs-MacBook-Air.local>
2024-10-14 20:26:28 +0100

    lab 2 complete

--- search.py
+++ search.py
@@ -226,10 +226,11 @@ if __name__ == "__main__":
     #        q_num=q_num+1
     # logger.info("Bool search results saved to txt")
 
+
     phrase_search_res=phrase_search(["income taxes"],json_index)
     # print(phrase_search_res)
     proximity_search_res=proximity_search(["#10(income,taxes)"],json_index)
-    print(proximity_search_res)
+    # print(proximity_search_res)
     # pp = pprint.PrettyPrinter(indent=4)
     # pp.pprint(search_res)
     
